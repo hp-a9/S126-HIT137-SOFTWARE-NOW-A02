ed272a63f56bb08a34c9c0d364a5d33116a4a122
hp-a9 <himanshuprabhat765@gmail.com>
2026-04-08 18:09:37 +0930

    Update question1.py
    Added function structure for Question 1

--- question1.py
+++ question1.py
@@ -1,1 +1,23 @@
+def shift_lowercase(ch, shift1, shift2, encrypt=True):
+    pass
 
+def shift_uppercase(ch, shift1, shift2, encrypt=True):
+    pass
+
+def transform_text(text, shift1, shift2, encrypt=True):
+    pass
+
+def encrypt_file(shift1, shift2):
+    pass
+
+def decrypt_file(shift1, shift2):
+    pass
+
+def verify_decryption():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
